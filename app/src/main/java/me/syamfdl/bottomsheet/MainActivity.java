b39c03caf5ad254c36b396393062ad7cfca27fab
syamfdl <syamfdl.lvlup@gmail.com>
2020-02-10 23:18:51 +0700

    bottom sheet

--- app/src/main/java/me/syamfdl/bottomsheet/MainActivity.java
+++ app/src/main/java/me/syamfdl/bottomsheet/MainActivity.java
@@ -1,14 +1,50 @@
 package me.syamfdl.bottomsheet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout mCustomBottomSheet;
+    private BottomSheetBehavior mBottomSheetBehavior;
+
+    private LinearLayout mHeaderLayout;
+    private ImageView mHeaderImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCustomBottomSheet = findViewById(R.id.bottom_sheet);
+        mBottomSheetBehavior = BottomSheetBehavior.from(mCustomBottomSheet);
+
+        mHeaderLayout = findViewById(R.id.header_layout);
+        mHeaderImage = findViewById(R.id.header_arrow);
+
+        mBottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                mHeaderImage.setRotation(slideOffset * 180);
+            }
+        });
+
+
+
+
     }
 }
